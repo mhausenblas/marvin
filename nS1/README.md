b54e075d0c42e9fc2da6e8fd465b143bf60089d7
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-05-28 18:32:17 +0100

    implements nS1 calender proxy

--- nS1/README.md
+++ nS1/README.md
@@ -2,9 +2,25 @@
 
 This nS is written in Go and takes a day as an input and emits events as timeframe/location key-value pairs:
 
-  day --> (timeframe:location)*
-  
+    day --> (timeframe:location)*
 
-## Prep
+## Build & Execute
 
-Need to do a `go get github.com/PuloV/ics-golang`
+As a preparation, you need to do:
+
+- `go get github.com/PuloV/ics-golang` to make the ics parser available
+- `go get github.com/Sirupsen/logrus` to make the logger available
+
+Then, build and execute:
+
+    $ go install && ../../../../bin/marvin-calproxy
+
+## Usage
+
+    $ http localhost:9999/events?date=2016-05-31
+    HTTP/1.1 200 OK
+    Content-Length: 57
+    Content-Type: application/javascript
+    Date: Sat, 28 May 2016 17:27:58 GMT
+
+    {"tf":"2016-05-31T09:30:00Z","loc":"Brussels\\, Belgium"}
--- nS1/src/github.com/mhausenblas/marvin-calproxy/main.go
+++ nS1/src/github.com/mhausenblas/marvin-calproxy/main.go
@@ -1,25 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	ics "github.com/PuloV/ics-golang"
+	log "github.com/Sirupsen/logrus"
+	"net/http"
 	"time"
 )
 
+const (
+	VERSION         string = "0.1.0"
+	DEFAULT_CAL_URL string = "https://calendar.google.com/calendar/ical/r5sj91351jcgb0gul5h0tvou7o%40group.calendar.google.com/public/basic.ics"
+	DAY_PARAM       string = "date"
+)
+
+var (
+	mux *http.ServeMux
+)
+
+type CalProxyResult struct {
+	Timeframe time.Time `json:"tf"`
+	Location  string    `json:"loc"`
+}
+
 func main() {
+	log.SetLevel(log.DebugLevel)
+	mux = http.NewServeMux()
+	fmt.Printf("This is the MARVIN nanoservice [Calendar Proxy] in version %s\n", VERSION)
 	parser := ics.New()
 	inputChan := parser.GetInputChan()
-	inputChan <- "https://calendar.google.com/calendar/ical/r5sj91351jcgb0gul5h0tvou7o%40group.calendar.google.com/public/basic.ics"
+	inputChan <- DEFAULT_CAL_URL
 	parser.Wait()
-	cal, err := parser.GetCalendars()
-	if err == nil {
-		t, _ := time.Parse(ics.YmdHis, "2016-05-31 00:00:00")
-		for _, calendar := range cal {
-			fmt.Println(calendar.GetName(), calendar.GetDesc())
-			eventsForDay, _ := calendar.GetEventsByDate(t)
-			for i, event := range eventsForDay {
-				fmt.Printf("№%d  %s \n", i, event)
+
+	mux.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		cal, err := parser.GetCalendars()
+		if err == nil {
+			day := r.URL.Query().Get(DAY_PARAM)
+			log.WithFields(log.Fields{"handle": "/"}).Info("Got day param ", day)
+			t, errp := time.Parse("2006-01-02", day)
+			if errp == nil {
+				log.WithFields(log.Fields{"handle": "/"}).Info("Looking up events for ", t)
+				for _, calendar := range cal {
+					eventsForDay, _ := calendar.GetEventsByDate(t)
+					log.WithFields(log.Fields{"handle": "/"}).Info("Got ", len(eventsForDay), " event(s)")
+					w.Header().Set("Content-Type", "application/javascript")
+					for _, e := range eventsForDay {
+						c := &CalProxyResult{
+							Timeframe: e.GetStart(),
+							Location:  e.GetLocation(),
+						}
+						cj, _ := json.Marshal(c)
+						fmt.Fprint(w, string(cj))
+					}
+				}
+			} else {
+				log.WithFields(log.Fields{"handle": "/"}).Error("Can't parse day due to ", errp)
 			}
 		}
-	}
+	})
+	log.Fatal(http.ListenAndServe(":9999", mux))
 }
