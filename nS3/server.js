fa310b88540e704989352417a61f1ea87ffd9275
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-05-30 06:46:39 +0100

    advances nS3: lookup

--- nS3/server.js
+++ nS3/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const http = require("http");
+const https = require("https");
 
 const PORT = 8787;
 
@@ -32,13 +34,44 @@ app.get('/rec', function(req, res) {
   res.json(events);
 });
 
+// lifted from http://stackoverflow.com/questions/9577611/http-get-request-in-node-js-express
+function getJSON(options, onResult) {
+  var prot = options.port == 443 ? https : http;
+  var req = prot.request(options, function(res){
+    var output = '';
+    console.log(options.host + ':' + res.statusCode);
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+            output += chunk;
+    });
+    res.on('end', function() {
+      var obj = JSON.parse(output);
+      onResult(res.statusCode, obj);
+    });
+  });
+  req.on('error', function(err) {
+  });
+  req.end();
+};
+
+
+
 function getEvents() {
   var lookupDate = new Date().toISOString().slice(0,10);
   var events;
-  // var pfts = getCloseByPTFs();
+  var eventsLookupCall = {
+      host: 'localhost',
+      port: 9999,
+      path: '/events?date='+lookupDate,
+      method: 'GET'
+  };
   console.log('Looking up events for today, that is: ' + lookupDate);
-  
+  getJSON(eventsLookupCall, function(statusCode, result) {
+    console.log(JSON.stringify(result));
+  });
+  // var pfts = getCloseByPTFs();
 }
 
 app.listen(PORT);
 console.log('This is MARVIN recommender running on http://localhost:' + PORT);
+//TODO: CAL SYNC
