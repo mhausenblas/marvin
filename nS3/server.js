618824d11b3a4c5033920d6e57f465a0a7df59d2
Michael Hausenblas <michael.hausenblas@gmail.com>
2016-05-31 19:41:04 +0200

    adds service discovery to PTFs/nS3

--- nS3/server.js
+++ nS3/server.js
@@ -35,6 +35,37 @@ app.get('/rec', function(req, res) {
   getEvents(res);
 });
 
+// Service discovery using Mesos-DNS
+function lookup(dpid, callback){
+  var dnspart = '';
+  var tmp = ' ';
+  var comp;
+  
+  console.log('Looking up service with DPID ' + dpid);
+  if(dpid.indexOf('/') >= 0){ // hierachical dpid like '/test/t0'
+    dnspart = dpid.substring(1);
+    comp = dnspart.split('/');
+    for (var i = comp.length-1; i >= 0; i--) {
+      tmp += comp[i] + "-";
+    }
+    dnspart = tmp.substring(0,tmp.length-1); // now it 't0-test'
+  }
+  else {
+    dnspart = dpid;
+  }
+  console.log('Extracted DNS part ' + dnspart);
+  getData('leader.mesos', 8123, '/v1/services/'+encodeURIComponent('_'+dnspart+'._tcp.marathon.mesos.'), function(err, resp){
+    var address = 'http://';
+    if (err) res.status(404).end();
+    else {
+      address += resp.ip + ':' + resp.port;
+      console.log('Resolved to address ' + address);
+      callback(resp.ip, resp.port);
+    }
+  });
+}
+
+// JSON HTTP data call
 function getData(host, port, path, callback){
   return http.get({
             host: host,
@@ -63,37 +94,42 @@ function getData(host, port, path, callback){
           });
 }
 
+// Looks up events and PTFs
 function getEvents(res) {
   var lookupDate = new Date().toISOString().slice(0,10); // extract the YYYY-MM-DD part
   var out = [];
   console.info('Looking up events for today, that is: ' + lookupDate);
-  getData('localhost', 9999, '/events?date='+lookupDate, function(err, events){
-    if (err) res.status(404).end();
-    else {
-      async.each(events,
-        function(event, callback){
-          var loc = event["loc"];
-          console.info('Looking at event ' + event["title"]);
-          if (loc != ''){
-            console.log('Looking up close-by public transport facilities for: ' + loc);
-            getData('localhost', 8989, '/closeby/'+encodeURIComponent(loc.trim()), function(err, closeby){
-              if (err) res.status(404).end();
-              else {
-                if (closeby){
-                  event.closeby = closeby;
-                }
-                out.push(event);
-                callback();
-              }
-            });
-          }
-        },
-        function(err){
-          res.json(out);
-          res.end();
-        });
+  lookup('/marvin/events', function(eIP, ePort){
+    getData(eIP, ePort, '/events?date='+lookupDate, function(err, events){
+      if (err) res.status(404).end();
+      else {
+        async.each(events,
+          function(event, callback){
+            var loc = event["loc"];
+            console.info('Looking at event ' + event["title"]);
+            if (loc != ''){
+              console.log('Looking up close-by public transport facilities for: ' + loc);
+              lookup('/marvin/osmlookup', function(oIP, oPort){
+                getData(oIP, oPort, '/closeby/'+encodeURIComponent(loc.trim()), function(err, closeby){
+                  if (err) res.status(404).end();
+                  else {
+                    if (closeby){
+                      event.closeby = closeby;
+                    }
+                    out.push(event);
+                    callback();
+                  }
+                });
+              });
+            }
+          },
+          function(err){
+            res.json(out);
+            res.end();
+          });
       }
     });
+  });
 }
 
 
